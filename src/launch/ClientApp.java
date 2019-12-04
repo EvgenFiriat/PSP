65bfe33d20d7ccbc8d3b97345205c170b46656bc
Yauheni Shymanovich <17m2000y@gmail.com>
2019-12-04 03:34:49 +0300

    [SMS]: Add EditUser Admin window

--- src/launch/ClientApp.java
+++ src/launch/ClientApp.java
@@ -1,7 +1,24 @@
 package launch;
 
-public class ClientApp {
-    public static void main(String[] args) {
+import com.sun.tools.javac.Main;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
+public class ClientApp extends Application {
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("../assets/fxml/Admin/CreateUserWindow/CreateUser.fxml"));
+        primaryStage.setTitle("SMS App");
+        primaryStage.getIcons().add(new Image(ClientApp.class.getResourceAsStream("../assets/img/icons/window-icon.png")));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 }
