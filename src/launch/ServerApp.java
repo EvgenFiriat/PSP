b2189e22b988397da21431acf25deb7f8361cc6b
Yauheni Shymanovich <17m2000y@gmail.com>
2019-12-04 15:18:35 +0300

    Add extra fxml

--- src/launch/ServerApp.java
+++ src/launch/ServerApp.java
@@ -1,7 +1,15 @@
 package launch;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class ServerApp {
     public static void main(String[] args) {
-
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            System.out.println("Connection successful!");
+        } catch (InstantiationException | InvocationTargetException | NoSuchMethodException | IllegalAccessException | ClassNotFoundException e) {
+            System.err.println("Fail...");
+            System.err.println(e);
+        }
     }
 }
